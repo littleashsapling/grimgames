a67a3a620e6d8cb62130b22c6fd04e4fa279c324
littleashsapling <59930398+littleashsapling@users.noreply.github.com>
2020-06-16 08:39:41 -0300

    first commit to git

---
+++ src/server.js
@@ -0,0 +1,68 @@
+//backend server
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+const apiPort = 3000
+    //database
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://admin:admin@grimgames-7mfso.mongodb.net/chatandrooms?retryWrites=true&w=majority', (err) => {
+    console.log('mongodb connected!', err)
+})
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
+
+//bodyparser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    })
+})
+
+
+app.get('/messages/:user', (req, res) => {
+    var user = req.params.user
+    Message.find({
+        name: user
+    }, (err, messages) => {
+        res.send(messages);
+    })
+})
+
+
+app.post('/messages', async(req, res) => {
+        try {
+            var message = new Message(req.body);
+
+            var savedMessage = await message.save()
+            console.log('saved');
+
+            var censored = await Message.findOne({
+                message: 'badword'
+            });
+            if (censored)
+                await Message.remove({
+                    _id: censored.id
+                })
+            else
+                io.emit('message', req.body);
+            res.sendStatus(200);
+        } catch (error) {
+            res.sendStatus(500);
+            return console.log('error', error);
+        } finally {
+            console.log('Message Posted')
+        }
+
+    })
+    //starting server
+app.listen(apiPort, () => console.log('Server running on port ${apiPort}'))
