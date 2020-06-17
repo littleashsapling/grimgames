79b09241d1e44068a7f7a4c43e510af34d2bb233
littleashsapling <59930398+littleashsapling@users.noreply.github.com>
2020-06-17 20:32:25 -0300

    dude idk

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 //backend server
 const path = require('path')
-const express = require('express')
 const http = require('http')
-var bodyParser = require('body-parser')
-    //static folder
+const express = require('express')
+const socketio = require('socket.io')
+
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
+    //static folder
 app.use(express.static(path.join(__dirname, 'front')))
-    //database
-const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://admin:admin@grimgames-7mfso.mongodb.net/chatandrooms?retryWrites=true&w=majority', (err) => {
+// Run when client connects
+io.on('connection', socket => {
+    console.log('New connection!')
+})
+
+//database
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://admin:admin@grimgames-7mfso.mongodb.net/chatandrooms?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
     console.log('mongodb connected!', err)
 })
 
 //starting server
 const PORT = 3000 || process.env.PORT
-app.listen(PORT, () => console.log('Server running on port ${PORT}'))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
